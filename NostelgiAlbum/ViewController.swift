15e9f5ded08903c0e3e0574a57f2129ebedbc22f
jeonmmingu <jeonalsrn@gmail.com>
2022-10-05 23:04:12 +0900

    Initial Commit

---
+++ NostelgiAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NostelgiAlbum
+//
+//  Created by 전민구 on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
