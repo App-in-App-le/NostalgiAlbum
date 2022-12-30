af60ba63e71873c138bc66d5db18f7b91157067b
chopmozzi <chopmojji@gmail.com>
2022-12-30 14:29:42 +0900

    gitignore 수정

---
+++ NostelgiAlbum/AlbumEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumEditViewController.swift
+//  NostelgiAlbum
+//
+//  Created by 황지웅 on 2022/12/30.
+//
+
+import UIKit
+
+class AlbumEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
