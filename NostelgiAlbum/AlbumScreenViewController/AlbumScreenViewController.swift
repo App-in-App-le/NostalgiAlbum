d076d49c1c343abe6a83e82e576840800f97b03c
jeonmmingu <jeonalsrn@gmail.com>
2023-02-13 13:02:01 +0900

    [2023/02/13.Mon] - Merge Conflict resolve

--- NostelgiAlbum/AlbumScreenViewController/AlbumScreenViewController.swift
+++ NostelgiAlbum/AlbumScreenViewController/AlbumScreenViewController.swift
@@ -191,9 +191,7 @@ extension AlbumScreenViewController:UICollectionViewDataSource{
             LongPressGestureRecognizer.picture = picture
             cell.pictureImgButton!.addGestureRecognizer(LongPressGestureRecognizer)
         } else {
-            cell.albumInit()
-            //print("indexPath : ",indexPath.item)
-            //print("count : ",data.count)
+            cell.albuminit()
             if (indexPath.item + pageNum * 2) > data.count {
                 cell.pictureImgButton!.isHidden = true
             }
