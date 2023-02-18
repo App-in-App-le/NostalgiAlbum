fd02433a5783fa4d2c80f1afc437e1f236d81ffe
jeonmmingu <jeonalsrn@gmail.com>
2023-02-18 16:31:23 +0900

    [2023/02/18.Sat] - AlbumScreenCollectionViewCell 코드 정리 & AlbumEditViewController 키보드 올라가는 기능 추가 / jeonmmingu

--- NostelgiAlbum/AlbumScreenViewController/AlbumScreenCollectionViewCell.swift
+++ NostelgiAlbum/AlbumScreenViewController/AlbumScreenCollectionViewCell.swift
@@ -2,13 +2,16 @@ import UIKit
 
 class AlbumScreenCollectionViewCell: UICollectionViewCell {
     
+    // - MARK: Declare Class' Properties
+    // Cell 하위에 들어가는 하위 뷰 객체들
     var pictureImgButton : UIButton?
     var pictureLabel: UILabel?
-    
+    // AlbumScreenViewController에서 받아온 정보들을 담을 객체
     var albumSVC: AlbumScreenViewController!
     var albumInfo : album!
     var albumCoverInfo : albumCover!
     
+    // - MARK: Set PictureImgButton
     func setButton(){
         pictureImgButton!.translatesAutoresizingMaskIntoConstraints = false
         pictureImgButton!.setImage(UIImage(systemName: "plus"), for: .normal)
@@ -18,6 +21,7 @@ class AlbumScreenCollectionViewCell: UICollectionViewCell {
         self.addSubview(pictureImgButton!)
     }
     
+    // - MARK: Set PictureLabel
     func setLabel(){
         pictureLabel!.translatesAutoresizingMaskIntoConstraints = false
         pictureLabel!.text = ""
@@ -25,7 +29,9 @@ class AlbumScreenCollectionViewCell: UICollectionViewCell {
         self.addSubview(pictureLabel!)
     }
     
-    func setLayout(HeightIsLongger scale: Bool){
+    // - MARK: Set Layout
+    func setLayout(HeightIsLonger scale: Bool){
+        // When height is longer than width
         if scale == true {
             NSLayoutConstraint.activate([
                 pictureImgButton!.leadingAnchor.constraint(equalTo: self.leadingAnchor),
@@ -41,7 +47,7 @@ class AlbumScreenCollectionViewCell: UICollectionViewCell {
             pictureImgButton!.frame = frame
             frame = CGRect(x: 240, y: 0, width: self.frame.width - 240, height: self.frame.height)
             pictureLabel!.frame = frame
-            
+            // when width is longer than height
         } else {
             NSLayoutConstraint.activate([
                 pictureImgButton!.topAnchor.constraint(equalTo: self.topAnchor),
@@ -60,31 +66,51 @@ class AlbumScreenCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    // - MARK: When the cell don't have picture Data, albumInit func is called.
     func albumInit() {
         reset()
         setButton()
         setLabel()
-        setLayout(HeightIsLongger: true)
+        setLayout(HeightIsLonger: true)
     }
     
+    // - MARK: When the cell has picture data, configure func is called.
     func configure(_ albumInfo : album){
-        reset()
         let totalPath = "\(albumInfo.AlbumTitle)_\(albumInfo.perAlbumIndex).png"
         let image = loadImageFromDocumentDirectory(imageName: totalPath, albumTitle: albumInfo.AlbumTitle)
-        
+        // Reset cell's subview to reload the data of collection view in albumScreenViewController.
+        reset()
         setButton()
         setLabel()
+        // Construct "Dynamic-Layout" by comparing the width and height of the image.
         if image!.size.width < image!.size.height {
-            setLayout(HeightIsLongger: true)
+            setLayout(HeightIsLonger: true)
         } else {
-            setLayout(HeightIsLongger: false)
+            setLayout(HeightIsLonger: false)
         }
-        
+        // Set image and text.
         pictureImgButton!.setImage(image!.resize(newWidth: pictureImgButton!.frame.width, newHeight: pictureImgButton!.frame.height), for: .normal)
-        
         pictureLabel!.text = albumInfo.ImageText
     }
     
+    // - MARK: The target action of pictureImgButton.
+    @objc func addPicutre() {
+        // When the pictureImageButton doesn't have a picture, go to the view where you can add the picture.
+        if pictureImgButton!.imageView?.image == UIImage(systemName: "plus"){
+            guard let editVC = self.albumSVC.storyboard?.instantiateViewController(withIdentifier: "AlbumEditViewController") as? AlbumEditViewController else { return }
+            editVC.index = self.albumSVC.coverIndex
+            editVC.albumCoverName = self.albumCoverInfo.albumName
+            editVC.collectionViewInAlbum = self.albumSVC.collectionView
+            editVC.modalPresentationStyle = .overFullScreen
+            // editVC.navigationController?.pushViewController(editVC, animated: false)
+            albumSVC.present(editVC, animated: false)
+            // When the pictureImageButton has a picture, go to the albumPicView.
+        } else {
+            picConfigure(albumInfo)
+        }
+    }
+    
+    // - MARK: When you touch a picture, go to the view where you can see the picture in detail.
     func picConfigure(_ albumInfo : album){
         guard let picVC = self.albumSVC.storyboard?.instantiateViewController(withIdentifier: "AlbumPicViewController") as? AlbumPicViewController else { return }
         picVC.picture = albumInfo
@@ -93,31 +119,17 @@ class AlbumScreenCollectionViewCell: UICollectionViewCell {
         albumSVC.present(picVC, animated: false)
     }
     
-    @objc func addPicutre() {
-        if pictureImgButton!.imageView?.image == UIImage(systemName: "plus"){
-            guard let editVC = self.albumSVC.storyboard?.instantiateViewController(withIdentifier: "AlbumEditViewController") as? AlbumEditViewController else { return }
-            editVC.index = self.albumSVC.coverIndex
-            editVC.albumCoverName = self.albumCoverInfo.albumName
-            editVC.collectionViewInAlbum = self.albumSVC.collectionView
-            editVC.modalPresentationStyle = .overFullScreen
-            editVC.navigationController?.pushViewController(editVC, animated: false)
-            albumSVC.present(editVC, animated: false)
-        } else {
-            picConfigure(albumInfo)
-        }
-    }
-    
+    // - MARK: Reset the properties of the subviews in cell and subviews.
     func reset() {
+        // Reset PictureImgButton
         pictureImgButton?.removeTarget(self, action: nil, for: .touchUpInside)
         pictureImgButton?.setImage(nil, for: .normal)
         pictureImgButton = nil
         pictureImgButton = UIButton()
-        
+        // Reset Picture Label
         pictureLabel?.text = nil
         pictureLabel = nil
         pictureLabel = UILabel()
     }
 }
 
-extension UIButton {
-}
